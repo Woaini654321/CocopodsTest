ece70e0795bb5ac82c4d46fb9b4428bd3033de1d
Woaini654321 <791903178@qq.com>
2018-12-13 10:03:59 +0800

    第二次修改

---
+++ Example/Pods/Target Support Files/CocopodsTest/CocopodsTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CocopodsTest : NSObject
+@end
+@implementation PodsDummy_CocopodsTest
+@end
---
+++ Example/Pods/Target Support Files/Pods-CocopodsTest_Example/Pods-CocopodsTest_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocopodsTest_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CocopodsTest_Example
+@end
